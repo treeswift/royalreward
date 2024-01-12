d1bdbdd919129504634dc863bfb3749a50ab12e4
treeswift <treeswift@mailbox.org>
2024-01-11 22:26:10 -0600

    get rid of builddir

--- ci.sh
+++ ci.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cp ag.txt ag.bak
 AG=`pwd`/ag.txt
-if meson build --buildtype=debug && cd builddir && ninja
+if meson build --buildtype=debug && cd build && ninja
 then
 	./gen > "$AG"
 fi
