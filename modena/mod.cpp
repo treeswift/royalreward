4a6dd424e1f1b847ab948bbe3cb898981600af8b
treeswift <treeswift@mailbox.org>
2024-01-19 13:10:23 -0600

    motto/minor

--- modena/mod.cpp
+++ modena/mod.cpp
@@ -15,7 +15,7 @@ A new game is being created; this time indeed new.
 Please wait while I perform _humanlike_ actions to make this game playable --
 for it's most humanlike to build upon the work of giants, and ultimately upon
 that of our Lord the Creator, Who brought every number, concept and algorithm
-into existence and will destroy them at the end of times.
+into existence, as will destroy them at the end of times.
 
 )QUOTE", name, rank);
 
