c31b422e38c2da5904627f28f2ba25180eb1d66c
treeswift <treeswift@mailbox.org>
2024-01-19 15:30:57 -0600

    military units

---
+++ algos/mission/military.h
@@ -0,0 +1,48 @@
+#ifndef _ALGOS_MISSION_MILITARY_H_
+#define _ALGOS_MISSION_MILITARY_H_
+
+#include <vector>
+
+namespace mil {
+
+enum class Unit {
+    Housewives,
+    Fairies,
+    Recruits,
+    Canines,
+    Scarecrows,
+    Lunatics,
+    Woodsfolk,
+    Goblins,
+    Bowmen,
+    Firstborn,
+    Spearmen,
+    Bedouins,
+    Minesfolk,
+    Specters,
+    Noblemen,
+    Cannibals,
+    Huns,
+    Kobolds,
+    Horsemen,
+    Wizards,
+    Illuminati,
+    Batmen,
+    Titans,
+    Jinns,
+    Salamanders,
+    Total
+};
+
+static_assert(25 == static_cast<int>(Unit::Total), "Exactly 5x5 troop types must be defined");
+
+struct Regiment {
+    Unit unit;
+    unsigned count;
+};
+
+using Army = std::vector<Regiment>;
+
+} // namespace mil
+
+#endif
