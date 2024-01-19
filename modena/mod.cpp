fab87c0eb0e1b8782d24a603e874debd30ad491b
treeswift <treeswift@mailbox.org>
2024-01-19 13:07:15 -0600

    motto refined

--- modena/mod.cpp
+++ modena/mod.cpp
@@ -13,9 +13,9 @@ int main(int argc, char** argv) {
 
 A new game is being created; this time indeed new.
 Please wait while I perform _humanlike_ actions to make this game playable --
-for it is most humanlike to build upon the work of giants, and ultimately upon
-that of our Lord the Creator, Who alone created numbers and all other concepts
-of mathematics and will destroy them at the end of times.
+for it's most humanlike to build upon the work of giants, and ultimately upon
+that of our Lord the Creator, Who brought every number, concept and algorithm
+into existence and will destroy them at the end of times.
 
 )QUOTE", name, rank);
 
