e70ba508f86f490cf08d0796da899f2c2904ca59
treeswift <treeswift@mailbox.org>
2024-01-23 21:31:25 -0600

    adjust CI tasks

--- ci.sh
+++ ci.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-cp ag.txt ag.bak
-AG=`pwd`/ag.txt
+# cp ag.txt ag.bak
+# AG=`pwd`/ag.txt
 if meson build && cd build && ninja
 then
-	time ./gen > "$AG"
+	# time ./gen > "$AG"
 	# time ./mod
-	./mod ~/Downloads/KB/kb_mod -1
+	time ./mod ~/Downloads/KB/kb_mod -1
 	./dat ~/Downloads/KB/kb_mod/Nolan/Nolan.DAT > ../Nolan.txt
 	cp ~/Downloads/KB/kb_mod/Nolan/Nolan.DAT ~/Downloads/KB/kb_mod/
 fi
