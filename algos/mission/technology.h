64b3f68b38af6c8cbc90c2343069aa4d5fb92621
treeswift <treeswift@mailbox.org>
2024-01-24 07:19:21 -0600

    tech ids

---
+++ algos/mission/technology.h
@@ -0,0 +1,26 @@
+#ifndef _ALGOS_MISSION_TECHNOLOGY_H_
+#define _ALGOS_MISSION_TECHNOLOGY_H_
+
+namespace dat {
+
+enum Technology {
+    Breed = 0,
+    Relocate,
+    Brimstone,
+    Fire,
+    Petrify,
+    Resuscitate,
+    Repentance,
+
+    Raft,
+    Stasis,
+    Sigint,
+    ToFort,
+    ToPort,
+    Summon,
+    Rally,
+};
+
+} // namespace dat
+
+#endif
