4ca73b45ef4f96e739c2911c868c55b9857bd1cb
treeswift <treeswift@mailbox.org>
2024-01-26 09:42:27 -0600

    asan in db.sh

--- db.sh
+++ db.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cp ag.txt ag.bak
 AG=`pwd`/ag.txt
-if meson build --buildtype=debug --reconfigure && cd build && ninja
+if meson build --buildtype=debug -Db_sanitize=address --reconfigure && cd build && ninja
 then
 	time ./gen > "$AG"
 fi
